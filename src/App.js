283da60560a79ab317a1c64f97050bc08d8ffdf4
Sammysoft <dareowonibisamuel@gmail.com>
2020-09-18 21:01:27 +0100

    more commits for frontend

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+   <div className={'app'}>
+     CCCSP NATIONAL WEBSITE
+   </div>
+  );
+}
+
+export default App;
